666cb565b9aada13f21663752f9428ac1ccb3368
Juan Andres Carmena <juan14nob@gmail.com>
2018-02-28 14:33:13 -0300

    add controller and model add readme instructions file

--- app/app.go
+++ app/app.go
@@ -3,6 +3,8 @@ package app
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"ServerGo/app/controllers"
+
 )
 
 var Server *echo.Echo
@@ -10,7 +12,6 @@ var Server *echo.Echo
 // init echo web server
 func Init() {
 	Server = echo.New()
-
 	// Middleware
 	Server.Use(middleware.Logger())
 	Server.Use(middleware.Recover())
@@ -21,6 +22,11 @@ func Init() {
 		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
 	}))
 
+	Server.Router()
+
 	//static file serviing
 	Server.Static("/static", "assets")
+
+
+	controllers.Users{}.Routes(Server)
 }
